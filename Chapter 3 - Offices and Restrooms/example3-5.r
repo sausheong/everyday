e2c09d81e9ab33ec6c8636ee883cebad27eefc3d
Philip Schweiger <schweiger@spotify.com>
2013-01-23 15:08:21 -0500

    Fixed some bugs in Chapter 3 R code:
    * Use 'ggsave' instead of 'pdf' (was not actually saving PDF files before)
    * replaced deprecated 'opts' with 'theme'
    * use 'stat="identity" in the geom_bar calls
    * use 'colMeans' instead of 'mean' for data frame

--- Chapter 3 - Offices and Restrooms/example3-5.r
+++ Chapter 3 - Offices and Restrooms/example3-5.r
@@ -1,19 +1,19 @@
 library(ggplot2)
 
-pdf("figure3-2.pdf")
 data <- read.table("simulation1.csv", header=TRUE, sep=",")
 mean <- colMeans(data)
 median <- apply(data,2,median)
 max  <- apply(data,2,max)
 df <- data.frame(population=seq(from=10,to=600,by=10),mean=mean, median=median,max=max)
 
-ggplot(data = df) + scale_shape_manual(name="Type", values=c(2,3,4)) + 
+ggplot(data = df) + scale_shape_manual(name="Type", values=c(2,3,4)) +
   geom_smooth(aes(x = population, y = mean)) +
   geom_point(aes(x  = population, y = mean, shape = "mean")) +
   geom_smooth(aes(x = population, y = median)) +
-  geom_point(aes(x  = population, y = median, shape = "median")) +  
+  geom_point(aes(x  = population, y = median, shape = "median")) +
   geom_smooth(aes(x = population, y = max)) +
   geom_point(aes(x  = population, y = max, shape = "max")) +
   scale_y_continuous("queue size") +
-  scale_x_continuous("population")  
-  
+  scale_x_continuous("population")
+
+ggsave("figure3-2.pdf")
--- Chapter 3 - Offices and Restrooms/example3-6.r
+++ Chapter 3 - Offices and Restrooms/example3-6.r
@@ -1,13 +1,14 @@
 library(ggplot2)
 
-pdf("figure3-4.pdf")
 data <- read.table("simulation1.csv", header=TRUE, sep=",")
 df <- data.frame(table(data$X70))
 colnames(df) <- c("queue_size", "frequency")
 percent_labels <- paste(df$frequency, '(', round(df$frequency*100/540, 2), '%)')
 
-ggplot(data=df) + opts(legend.position = "none") + scale_fill_grey(start = 0.6, end = 0.8) + 
-  geom_bar(aes(x = queue_size, y = frequency, fill = factor(queue_size))) +
+ggplot(data=df) + theme(legend.position = "none") + scale_fill_grey(start = 0.6, end = 0.8) + 
+  geom_bar(aes(x = queue_size, y = frequency, fill = factor(queue_size)), stat="identity") +
   geom_text(aes(x = queue_size, y = frequency, label = percent_labels, size=1)) +
   scale_y_continuous("frequency") +
   scale_x_discrete("queue size")
+
+ggsave("figure3-4.pdf")
