3296c2e730bfb7c504efc539ba4a75b7e3bf7959
Yury Kotlyarov <yura@brainhouse.ru>
2014-01-23 06:44:08 +0700

    the same issue as at #1

--- Chapter 5 - Discover Yourself Through Email/mails_by_hour.r
+++ Chapter 5 - Discover Yourself Through Email/mails_by_hour.r
@@ -11,7 +11,7 @@ sent_count <- times_count(times=sent_data['date'], element='%H')
 hours_of_day <- c("00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23")
 df <- data.frame(hours=factor(hours_of_day, levels=hours_of_day),inbox=inbox_count,sent=sent_count)
 
-ggplot(data=df) + scale_shape_manual(name="Mailbox", value=c(2,3)) +
+ggplot(data=df) + scale_shape_manual(name="Mailbox", values=c(2,3)) +
   geom_point(aes(x=hours,y=inbox, shape='inbox')) +
   geom_smooth(aes(x=hours,y=inbox, group=1)) +
   geom_point(aes(x=hours,y=sent, shape='sent')) +
